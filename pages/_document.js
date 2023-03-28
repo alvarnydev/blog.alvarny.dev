18b9d39268f121c7b8cfc00abbe2cb2fbbab3034
Yannik Sahl <yanniksahl@outlook.de>
2023-03-28 09:32:22 +0200

    Favicon fixes

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="theme-compiled">
         <Head />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
