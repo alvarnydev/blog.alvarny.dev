e7c8de1f6c2168c9ae5ead1afc4f36425cde901d
alvarnydev <me@alvarny.dev>
2023-04-18 15:42:28 +0200

    Made values fixed that were previously determined by env

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Alvarny';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : "This Week's Additions";
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Hope you found something useful!';
 
   return {
     name,
